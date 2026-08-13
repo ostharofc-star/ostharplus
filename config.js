a67e03cfe885257bf5a75204dc4cd5e8311849ae
ostharofc-star <ostharofc@gmail.com>
2026-08-13 15:47:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hQI0QITC#H3RP_SC1lcfDIAYZkSDR-BZuOkDEf1nhwE9vY3hMsFM",
+SESSION_ID: process.env.SESSION_ID || "NYgGDJZK#cWac9zjGIRcq6_Ec32_wUZ8ZO1QaeHS5FlZBTREXXl8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 OSTHAR-PLUS Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
