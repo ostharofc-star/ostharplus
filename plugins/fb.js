8ff7dbcd22b8d15bd8d9354eba2691ffb34a1454
ostharofc-star <ostharofc@gmail.com>
2026-08-13 22:40:27 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -11,7 +11,7 @@ cmd(
     filename: __filename,
   },
   async (
-    danuwa,
+    OSTHAR,
     mek,
     m,
     {
@@ -63,18 +63,18 @@ Your fb video
 👻 *Quality*: ${qualityText}
 `;
 
-      await danuwa.sendMessage(
+      await OSTHAR.sendMessage(
         from,
         {
           image: {
-            url: "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/fbdownloader.png?raw=true",
+            url: "https://github.com/ostharofc-star/ostharplus/blob/main/images/a_please_remove_this_t%20(1).png?raw=true",
           },
           caption: desc,
         },
         { quoted: mek }
       );
 
-      await danuwa.sendMessage(
+      await OSTHAR.sendMessage(
         from,
         {
           video: { url: bestQualityUrl },
@@ -83,7 +83,7 @@ Your fb video
         { quoted: mek }
       );
 
-      return reply("Thank you for using DANUWA-MD");
+      return reply("Thank you for using OSTHAR-PLUS");
     } catch (e) {
       console.error(e);
       reply(`*Error:* ${e.message || e}`);
