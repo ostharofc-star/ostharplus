317d17c41dc3f1560ef5dc9912276745d2da742f
ostharofc-star <ostharofc@gmail.com>
2026-08-13 15:51:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "NYgGDJZK#cWac9zjGIRcq6_Ec32_wUZ8ZO1QaeHS5FlZBTREXXl8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 OSTHAR-PLUS Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94723853792',  // Replace with the owner's phone number
 
 
 
